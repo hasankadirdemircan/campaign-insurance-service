45c817fcca33a91426cba32ce005eb9691a1679b
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2023-05-05 21:39:41 +0300

    initialize commit

---
+++ src/test/java/com/allianz/insurance/InsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
