b0a67f2ac47abb76008f46963be8169a9489ffb2
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2023-05-09 00:11:09 +0300

    updated swwagger doc

--- src/main/java/com/allianz/insurance/controller/CampaignController.java
+++ src/main/java/com/allianz/insurance/controller/CampaignController.java
@@ -5,10 +5,7 @@ import com.allianz.insurance.response.CampaignHistoryResponse;
 import com.allianz.insurance.response.CampaignResponse;
 import com.allianz.insurance.response.CampaignStatisticsResponse;
 import com.allianz.insurance.service.CampaignService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -32,11 +29,17 @@ public class CampaignController {
     @ApiResponses(
             value = {
                     @ApiResponse(code = 201, message = "Successful Create Campaign"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid and therefore the campaign has not been created.", response = String.class),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @PostMapping("/campaigns")
-    public CampaignResponse createCampaign(@RequestHeader(name="Authorization") String jwt, @Valid CreateCampaignRequest request) {
+    public CampaignResponse createCampaign(
+            @ApiParam(
+                    value = "JWT Token" ,
+                    example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt, @Valid CreateCampaignRequest request) {
         return campaignService.createCampaign(jwt, request);
     }
 
@@ -44,11 +47,17 @@ public class CampaignController {
     @ApiResponses(
             value = {
                     @ApiResponse(code = 200, message = "Successful Activated the Campaign"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid", response = String.class),
+                    @ApiResponse(code = 404, message = "The campaign is not found"),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @PutMapping("/campaigns/activate/{campaignID}")
-    public CampaignResponse activateCampaign(@RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
+    public CampaignResponse activateCampaign(@ApiParam(
+            value = "JWT Token" ,
+            example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
         return campaignService.activateCampaign(jwt, campaignID);
     }
 
@@ -56,11 +65,18 @@ public class CampaignController {
     @ApiResponses(
             value = {
                     @ApiResponse(code = 200, message = "Successful Deactivated the Campaign"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid", response = String.class),
+                    @ApiResponse(code = 404, message = "The campaign is not found"),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @PutMapping("/campaigns/deactivate/{campaignID}")
-    public CampaignResponse deactivateCampaign(@RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
+    public CampaignResponse deactivateCampaign(
+            @ApiParam(
+                    value = "JWT Token" ,
+                    example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
         return campaignService.deactivateCampaign(jwt, campaignID);
     }
 
@@ -68,11 +84,18 @@ public class CampaignController {
     @ApiResponses(
             value = {
                     @ApiResponse(code = 200, message = "Successful Get campaign"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid", response = String.class),
+                    @ApiResponse(code = 404, message = "The campaign is not found"),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @GetMapping("/campaigns/{campaignID}")
-    public CampaignResponse getCampaignById(@RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
+    public CampaignResponse getCampaignById(
+            @ApiParam(
+                    value = "JWT Token" ,
+                    example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
         return campaignService.findCampaignById(jwt, campaignID);
     }
 
@@ -80,11 +103,18 @@ public class CampaignController {
     @ApiResponses(
             value = {
                     @ApiResponse(code = 200, message = "Successful Get all campaign"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid", response = String.class),
+                    @ApiResponse(code = 404, message = "The campaign is not found"),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @GetMapping("/campaigns")
-    public List<CampaignResponse> findAllCampaign(@RequestHeader(name="Authorization") String jwt) {
+    public List<CampaignResponse> findAllCampaign(
+            @ApiParam(
+                    value = "JWT Token" ,
+                    example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt) {
         return campaignService.findAllCampaign(jwt);
     }
 
@@ -92,24 +122,37 @@ public class CampaignController {
     @ApiResponses(
             value = {
                     @ApiResponse(code = 200, message = "Successful Get Campaign History by campaignID"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid", response = String.class),
+                    @ApiResponse(code = 404, message = "The campaign is not found"),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @GetMapping("/campaigns/history/{campaignID}")
-    public CampaignHistoryResponse findCampaignHistoryById(@RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
+    public CampaignHistoryResponse findCampaignHistoryById(
+            @ApiParam(
+                    value = "JWT Token" ,
+                    example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt, @PathVariable(value = "campaignID") Long campaignID) {
         return campaignService.findCampaignHistoryById(jwt, campaignID);
     }
-
 
     @ApiOperation(value = "Get All Campaign Statics", response = CampaignResponse.class)
     @ApiResponses(
             value = {
-                    @ApiResponse(code = 200, message = "Successful Get Campaign History by campaignID"),
-                    @ApiResponse(code = 500, message = "Internal Server Error"),
+                    @ApiResponse(code = 200, message = "Successful Get Campaign Statics"),
+                    @ApiResponse(code = 400, message = "The Campaign request is invalid", response = String.class),
+                    @ApiResponse(code = 404, message = "The campaign is not found"),
+                    @ApiResponse(code = 415, message = "The content type is unsupported"),
+                    @ApiResponse(code = 500, message = "An unexpected error has occurred. The error has been logged and is being investigated."),
             }
     )
     @GetMapping("/campaigns/statistics")
-    public List<CampaignStatisticsResponse> getCampaignStatistics(@RequestHeader(name="Authorization") String jwt) {
+    public List<CampaignStatisticsResponse> getCampaignStatistics(
+            @ApiParam(
+                    value = "JWT Token" ,
+                    example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJkZW1pcmNhbmhhc2Fua2FkaXJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.gj0OOwdIFtyS3L32NQ2hSGP1c6MqZLFZbk8isg3tqpM")
+            @RequestHeader(name="Authorization") String jwt) {
         return campaignService.getCampaignStatistics(jwt);
     }
 }
